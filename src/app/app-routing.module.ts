a62715370cbaae862e56745e024ad794d0a3aa96
unknown <bylasuhaimi@gmail.com>
2020-07-29 14:25:25 +0800

    Ionic 5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then( m => m.AboutPageModule)
   },
 ];
 
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { TodoService } from '../todo.service';
+import { Todo } from '../todo';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,27 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public todos: Array<Todo> = [];
+  async ngOnInit(){
 
+    this.todos = await this.todoService.read();
+    
+
+  }
+  constructor(public todoService: TodoService) {}
+  getIcon(todo: { completed: any; }){
+    if(todo.completed) return 'checkmark-circle';
+    else return 'stopwatch';
+  }
+  public async createTodo(){
+    let key = await this.todoService.generateKey();
+    let todo = {
+      title: `${key}`,
+      note: "A new todo",
+      completed: true
+    };
+    await this.todoService.create(key,todo);
+    this.todos = await this.todoService.read();
+  }
+  
 }
